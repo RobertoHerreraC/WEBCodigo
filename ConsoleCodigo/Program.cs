5f91dae9d1adb6fa04563b144ce7a9148772ce79
Roberto Herrera <robert_hc_95@hotmail.com>
2024-09-04 23:02:57 -0500

    Add project files.

---
+++ ConsoleCodigo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCodigo;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await InsertProduct();
+        Console.Read();
+
+    }
+
+
+    private static async Task InsertProduct()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string url = "https://localhost:7227/api/Products/Insert";
+
+            var product = new
+            {
+                productID = 0,
+                price = 12.50,
+                name = "Test",
+                isActive = true,
+                createDate = DateTime.Now
+            };
+            var json = JsonSerializer.Serialize(product);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Product inserted successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+
+
+    private static async Task GetCustomers()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            // URL del servicio
+            string url = "https://localhost:7227/api/Customers/GetByFilters";
+
+            //Guardar response
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            // Verificar si la respuesta fue exitosa
+            if (response.IsSuccessStatusCode)
+            {
+                // Leer y deserializar el contenido de la respuesta
+                List<Customer> customers = await response.Content.ReadFromJsonAsync<List<Customer>>();
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"ID: {customer.CustomerID}, Name: {customer.Name}, Document: {customer.DocumentNumber}, Type: {customer.DocumentType}, Active: {customer.IsActive}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Llamada con error");
+
+            }
+        }
+    }
+
+}
---
+++ WEBCodigo/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+using WEBCodigo.Models;
+
+namespace WEBCodigo.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public ProductsController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string url = "https://localhost:7227/api/Products/GetByFilters";
+            List<Product> products = new List<Product>();
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                products = await response.Content.ReadFromJsonAsync<List<Product>>();
+            }
+            else
+            {
+                ViewBag.Error = $"Error: {response.StatusCode}";
+            }
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(Product product)
+        {
+           
+            string url = "https://localhost:7227/api/Products/Insert";
+
+            var json = JsonSerializer.Serialize(product);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _httpClient.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Message = "Product inserted successfully.";
+            }
+            else
+            {
+                ViewBag.Error = $"Error: {response.StatusCode}";
+            }
+            return View();            
+        }
+    }
+}
